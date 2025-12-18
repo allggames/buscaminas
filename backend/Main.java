ae2a004d74c9e462e7730d14cc20e60a330c25bb
TINI <agostinaanahi2001@gmail.com>
2025-12-18 16:27:20 -0300

    Implement basic HTTP server in Main.java

---
+++ backend/Main.java
@@ -0,0 +1,24 @@
+package backend;
+
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+
+        server.createContext("/", exchange -> {
+            String response = "<html><body><h1>Buscaminas</h1><p>Cargando el juego...</p></body></html>";
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
+
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Servidor iniciado en http://localhost:8000");
+    }
+}
